81ec6a149dfeb169cec5ac5df332f42d6a89bfce
Mahesh Umale <mumale@gmail.com>
2025-09-30 17:55:59 +0530

    FIRST NEW COMMIT FOR PROJECT

---
+++ createGIT.bat
@@ -0,0 +1,7 @@
+echo "# BREAKOUT_SCANNER_WEBAPP" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/MaheshUmale/BREAKOUT_SCANNER_WEBAPP.git
+git push -u origin main
